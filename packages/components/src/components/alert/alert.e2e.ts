39aee6309aa07ec2782e03b4c0d59b9d15c0c867
Stefan Dietz <mail@stefan-dietz.eu>
2024-12-05 15:14:47 +0100

    Alert: Adjust E2E test structure
    Refs: #6987

--- packages/components/src/components/alert/alert.e2e.ts
+++ packages/components/src/components/alert/alert.e2e.ts
@@ -2,31 +2,35 @@ import { expect } from '@playwright/test';
 import { test } from '@stencil/playwright';
 
 test.describe('kol-alert', () => {
-	test('should emit "close" when close button is clicked', async ({ page }) => {
-		await page.setContent('<kol-alert _label="Alert" _has-closer />');
-		const eventPromise = page.locator('kol-alert').evaluate(async (element: HTMLKolAlertElement) => {
-			return new Promise((resolve) => {
-				element.addEventListener('close', resolve);
+	test.describe('Callbacks', () => {
+		test('should call "onClose" when the close button is clicked', async ({ page }) => {
+			await page.setContent('<kol-alert _label="Alert" _has-closer />');
+			const eventPromise = page.locator('kol-alert').evaluate(async (element: HTMLKolAlertElement) => {
+				return new Promise((resolve) => {
+					element._on = {
+						onClose: (_event: Event, value?: unknown) => {
+							resolve(value);
+						},
+					};
+				});
 			});
+			await page.waitForChanges();
+			await page.getByTestId('alert-close-button').click();
+			await expect(eventPromise).resolves.toBeUndefined();
 		});
-		await page.waitForChanges();
-		await page.getByTestId('alert-close-button').click();
-		await expect(eventPromise).resolves.toBeTruthy();
 	});
 
-	test('should call "onClose" when the close button is clicked', async ({ page }) => {
-		await page.setContent('<kol-alert _label="Alert" _has-closer />');
-		const eventPromise = page.locator('kol-alert').evaluate(async (element: HTMLKolAlertElement) => {
-			return new Promise((resolve) => {
-				element._on = {
-					onClose: (_event: Event, value?: unknown) => {
-						resolve(value);
-					},
-				};
+	test.describe('DOM events', () => {
+		test('should emit "close" when close button is clicked', async ({ page }) => {
+			await page.setContent('<kol-alert _label="Alert" _has-closer />');
+			const eventPromise = page.locator('kol-alert').evaluate(async (element: HTMLKolAlertElement) => {
+				return new Promise((resolve) => {
+					element.addEventListener('close', resolve);
+				});
 			});
+			await page.waitForChanges();
+			await page.getByTestId('alert-close-button').click();
+			await expect(eventPromise).resolves.toBeTruthy();
 		});
-		await page.waitForChanges();
-		await page.getByTestId('alert-close-button').click();
-		await expect(eventPromise).resolves.toBeUndefined();
 	});
 });
