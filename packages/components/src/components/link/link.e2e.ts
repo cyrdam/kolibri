faa646c93aa26004db5d8d75955e0a4ec2d935b9
Stefan Dietz <mail@stefan-dietz.eu>
2024-12-07 00:04:24 +0100

    Link: Add DOM events and E2E tests
    Refs: #6987

---
+++ packages/components/src/components/link/link.e2e.ts
@@ -0,0 +1,42 @@
+import { expect } from '@playwright/test';
+import { test } from '@stencil/playwright';
+
+test.describe('kol-link', () => {
+	test.describe('Callbacks', () => {
+		test(`should call onClick when internal anchor emits click`, async ({ page }) => {
+			await page.setContent('<kol-link _label="Link"></kol-link>');
+			const kolLink = page.locator('kol-link');
+
+			const eventPromise = kolLink.evaluate((element: HTMLKolLinkElement) => {
+				return new Promise<void>((resolve) => {
+					element._on = {
+						onClick: () => {
+							resolve();
+						},
+					};
+				});
+			});
+			await page.waitForChanges();
+
+			await page.locator('a').dispatchEvent('click');
+			await expect(eventPromise).resolves.toBeUndefined();
+		});
+	});
+
+	test.describe('DOM events', () => {
+		test(`should emit click when internal anchor emits click`, async ({ page }) => {
+			await page.setContent('<kol-link _label="Link"></kol-link>');
+			const eventPromise = page.locator('kol-link').evaluate(async (element: HTMLKolLinkElement) => {
+				return new Promise<void>((resolve) => {
+					element.addEventListener('click', () => {
+						resolve();
+					});
+				});
+			});
+			await page.waitForChanges();
+			await page.locator('a').dispatchEvent('click');
+
+			await expect(eventPromise).resolves.toBeUndefined();
+		});
+	});
+});
