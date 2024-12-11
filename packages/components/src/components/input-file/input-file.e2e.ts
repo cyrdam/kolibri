2df761b801067d8d2e41ec4230ae5c3d52794e48
Stefan Dietz <mail@stefan-dietz.eu>
2024-12-11 21:59:51 +0100

    Add DOM even tests for input-file
    Refs: #6987

--- packages/components/src/components/input-file/input-file.e2e.ts
+++ packages/components/src/components/input-file/input-file.e2e.ts
@@ -2,6 +2,8 @@ import { test } from '@stencil/playwright';
 import { testInputCallbacksAndEvents } from '../../e2e';
 import type { FillAction } from '../../e2e/utils/FillAction';
 import { expect } from '@playwright/test';
+import { Callback } from '../../schema/enums';
+import { KolEvent } from '../../utils/events';
 
 const COMPONENT_NAME = 'kol-input-file';
 const TEST_VALUE: [] = [];
@@ -17,12 +19,12 @@ test.describe(COMPONENT_NAME, () => {
 	testInputCallbacksAndEvents<HTMLKolInputFileElement>(COMPONENT_NAME, TEST_VALUE, fillAction, ['input', 'change']);
 
 	test.describe('Callbacks', () => {
-		['onInput', 'onChange'].forEach((callbackName) => {
-			test(`should call ${callbackName} when internal input receives file selection`, async ({ page }) => {
+		[Callback.onInput, Callback.onChange].forEach((callbackName) => {
+			test(`should call ${callbackName} callback when internal input receives file selection`, async ({ page }) => {
 				await page.setContent(`<kol-input-file _label="Input"></kol-input-file>`);
 				const component = page.locator(COMPONENT_NAME);
 
-				const filelistPromise = component.evaluate((element: HTMLKolInputFileElement, callbackName) => {
+				const fileList = component.evaluate((element: HTMLKolInputFileElement, callbackName) => {
 					return new Promise<FileList>((resolve) => {
 						element._on = {
 							[callbackName]: (_event: InputEvent, value: FileList) => {
@@ -36,7 +38,30 @@ test.describe(COMPONENT_NAME, () => {
 				await fillAction(page);
 				await page.waitForChanges();
 
-				expect(Object.keys(await filelistPromise)).toHaveLength(1); // no great way to test this, because Playwright has no FileList implementation.
+				expect(Object.keys(await fileList)).toHaveLength(1); // no great way to test this, because Playwright has no FileList implementation.
+			});
+		});
+	});
+
+	test.describe('DOM events', () => {
+		[KolEvent.input, KolEvent.change].forEach((eventName) => {
+			test(`should emit ${eventName} when internal input receives file selection`, async ({ page }) => {
+				await page.setContent(`<kol-input-file _label="Input"></kol-input-file>`);
+				const component = page.locator(COMPONENT_NAME);
+
+				const fileList = component.evaluate((element: HTMLKolInputFileElement, eventName) => {
+					return new Promise<FileList>((resolve) => {
+						element.addEventListener(eventName, (event: Event) => {
+							resolve((event as CustomEvent).detail as FileList);
+						});
+					});
+				}, eventName);
+				await page.waitForChanges();
+
+				await fillAction(page);
+				await page.waitForChanges();
+
+				expect(Object.keys(await fileList)).toHaveLength(1); // no great way to test this, because Playwright has no FileList implementation.
 			});
 		});
 	});
