32a2149fc341c6f4602ea44bdfe1a51a133cb3eb
Stefan Dietz <mail@stefan-dietz.eu>
2025-01-15 16:26:21 +0100

    Update sample app cases
    Refs: #7178

--- packages/samples/react/src/components/input-color/partials/cases.tsx
+++ packages/samples/react/src/components/input-color/partials/cases.tsx
@@ -1,10 +1,9 @@
 import React, { forwardRef } from 'react';
-
 import { KolInputColor } from '@public-ui/react';
+import type { Components } from '@public-ui/components';
 
 import { ERROR_MSG } from '../../../shares/constants';
 
-import type { Components } from '@public-ui/components';
 export const InputColorCases = forwardRef<HTMLKolInputColorElement, Components.KolInputColor>(function InputColorCases(props, ref) {
 	return (
 		<div className="grid gap-4">
@@ -26,6 +25,9 @@ export const InputColorCases = forwardRef<HTMLKolInputColorElement, Components.K
 				_suggestions="['#000000','#f08080', '#0000ff','#00ff00']"
 				_touched
 			/>
+			<KolInputColor {...props} _msg={{ _type: 'info', _description: 'Just a hint' }} _label="Color" />
+			<KolInputColor {...props} _msg={{ _type: 'warning', _description: 'Small warning' }} _label="Color" />
+			<KolInputColor {...props} _msg={{ _type: 'success', _description: 'Success message' }} _label="Color" />
 			<KolInputColor {...props} ref={ref} _accessKey="C" _hint="Hint text" _label="Color with hint" _value="#f08080" />
 			<KolInputColor {...props} _disabled _label="Color (Disabled)" _value="#f08080" />
 			<KolInputColor {...props} _label="With access key" _accessKey="c"></KolInputColor>
--- packages/samples/react/src/components/input-date/partials/cases.tsx
+++ packages/samples/react/src/components/input-date/partials/cases.tsx
@@ -1,10 +1,9 @@
 import React, { forwardRef } from 'react';
-
 import { KolInputDate } from '@public-ui/react';
-
-import { ERROR_MSG } from '../../../shares/constants';
-
 import type { Components } from '@public-ui/components';
+
+import { ERROR_MSG, HINT_MSG } from '../../../shares/constants';
+
 export const InputDateCases = forwardRef<HTMLKolInputDateElement, Components.KolInputDate>(function InputDateCases(props, ref) {
 	return (
 		<div className="grid gap-4">
@@ -16,11 +15,15 @@ export const InputDateCases = forwardRef<HTMLKolInputDateElement, Components.Kol
 				{...props}
 				_step={1}
 				_type="datetime-local"
+				_hint={HINT_MSG}
 				_msg={{ _type: 'error', _description: ERROR_MSG }}
 				_label="Local datetime (with seconds)"
 				_required
 				_touched
 			/>
+			<KolInputDate {...props} _msg={{ _type: 'info', _description: 'Just a hint' }} _label="Color" />
+			<KolInputDate {...props} _msg={{ _type: 'warning', _description: 'Small warning' }} _label="Color" />
+			<KolInputDate {...props} _msg={{ _type: 'success', _description: 'Success message' }} _label="Color" />
 			<KolInputDate {...props} _type="month" _label="Month" _required />
 			<KolInputDate {...props} ref={ref} _accessKey="W" _type="week" _label="Week" _required />
 			<KolInputDate {...props} _type="time" _label="Time (standard)" _required />
