18d215c1f63d8bb19233c2b4d5219ceef4d96d97
Stefan Dietz <mail@stefan-dietz.eu>
2025-01-17 09:53:04 +0100

    Fix e2e
    Refs: #7178

--- packages/components/src/components/input-password/shadow.tsx
+++ packages/components/src/components/input-password/shadow.tsx
@@ -120,6 +120,7 @@ export class KolInputPassword implements InputPasswordAPI, FocusableElement {
 			<KolIconButtonFc
 				componentName="button"
 				class="kol-input-password__password-toggle-button"
+				data-testid="kol-input-password-toggle-button"
 				label={this._passwordVisible ? translate('kol-hide-password') : translate('kol-show-password')}
 				variant="ghost"
 				onClick={(): void => {
